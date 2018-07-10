9ecba504b6ed63922b12f100b80781bbb0c8121f
gbox3d <gbox3d@gmail.com>
2018-07-10 15:18:43 +0900

    map tool(async input process)

--- day12/exam1/maptool.cpp
+++ day12/exam1/maptool.cpp
@@ -4,11 +4,19 @@
 #include "maptool.h"
 
 COORD g_cdCurrentCursorPos;
+WCHAR g_wcCurrentBrushCode;
+WORD g_wCurrentBrushAttr;
+
+COORD _oldPos = { -1,-1 };
+WCHAR _oldCode = -1;
+WORD _oldAttr = -1;
 
 void initMapTool() 
 {
 	g_cdCurrentCursorPos.X = 40;
 	g_cdCurrentCursorPos.Y = 12;
+	g_wcCurrentBrushCode = 0x20;
+	g_wCurrentBrushAttr = 0x00e0;
 }
 
 int parseCmd(char *szCmdBuf)
@@ -30,9 +38,6 @@ int parseCmd(char *szCmdBuf)
 	}
 	else if (!strcmp(szTokenBuf[0], "moveCursor")) {
 
-		static COORD _oldPos = { -1,-1 };
-		static WCHAR _oldCode = -1;
-		static WORD _oldAttr = -1;
 		if (_oldPos.X >= 0) {
 			TGE::setCharacter(TGE::g_chiBuffer,  _oldPos.X, _oldPos.Y,
 				_oldCode, _oldAttr);
@@ -56,6 +61,25 @@ int parseCmd(char *szCmdBuf)
 		//TGE::clearScreenBuffer(_code,_attr)
 		TGE::clearScreenBuffer(0x2e, 0x0007);
 	}
+	else if (!strcmp(szTokenBuf[0], "changeBrush")) {
+		WCHAR _code = (WCHAR)strtol(szTokenBuf[1], NULL, 16);
+		WORD _attr = (WORD)strtol(szTokenBuf[2], NULL, 16);
+		g_wcCurrentBrushCode = _code;
+		g_wCurrentBrushAttr = _attr;
+	}
+	else if (!strcmp(szTokenBuf[0], "saveb")) {
+		FILE *fp;
+		fopen_s(&fp,szTokenBuf[1], "w");
+		fwrite(TGE::g_chiBuffer, 2000 * sizeof(CHAR_INFO), 1, fp);
+		fclose(fp);
+	}
+	else if (!strcmp(szTokenBuf[0], "loadb")) {
+		FILE *fp;
+		fopen_s(&fp, szTokenBuf[1], "r");
+		fread_s(TGE::g_chiBuffer,2000 * sizeof(CHAR_INFO), sizeof(CHAR_INFO), 2000, fp);
+		fclose(fp);
+	}
+
 
 	return 1;
 }
--- day12/exam1/maptool.h
+++ day12/exam1/maptool.h
@@ -4,3 +4,7 @@ void initMapTool();
 int parseCmd(char *szCmdBuf);
 
 extern COORD g_cdCurrentCursorPos;
+extern COORD _oldPos;
+
+extern WCHAR g_wcCurrentBrushCode;
+extern WORD g_wCurrentBrushAttr;
