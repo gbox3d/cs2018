3d048c577972f4aeb89c9e08349d97ebc1864ddf
gbox3d <gbox3d@gmail.com>
2018-07-16 15:27:33 +0900

    화염방사기 구현

--- day16/exam1/gameObject.cpp
+++ day16/exam1/gameObject.cpp
@@ -16,6 +16,7 @@ namespace cs2018prj {
 			pObj->m_nFSM = 0;
 			pObj->m_pSprite = pSpr;
 			pObj->m_bActive = true;
+			pObj->m_pWepon = NULL;
 			//pObj->m_Spr = _spr;
 		}
 
@@ -33,6 +34,20 @@ namespace cs2018prj {
 			if (TGE::input::g_KeyTable[VK_DOWN]) {
 				pObj->m_posy += (pObj->m_dbSpeed * _deltaTick);
 			}
+
+			if (TGE::input::g_KeyTable['F']) {
+				if (pObj->m_pWepon) {
+					cs2018prj::attackObject_frameBurster::S_GAMEOBJECT *pWepon =
+						(cs2018prj::attackObject_frameBurster::S_GAMEOBJECT *)pObj->m_pWepon;
+					if (pWepon->m_nFSM == 0) {
+						pWepon->m_nFSM = 10;
+						pWepon->m_posx = pObj->m_posx;
+						pWepon->m_posy = pObj->m_posy - 2;
+					}
+				}
+			}
+
+
 
 			switch (pObj->m_nFSM)
 			{
@@ -147,6 +162,12 @@ namespace cs2018prj {
 
 			}
 				break;
+			case 100:
+				pObj->m_bActive = false;
+				pObj->m_nFSM = 101;
+				break;
+			case 101:
+				break;
 			default:
 				break;
 			}
@@ -154,9 +175,11 @@ namespace cs2018prj {
 
 		void Render(S_GAMEOBJECT *pObj, CHAR_INFO *pTargetBuf)
 		{	
-			tge_sprite::put(pObj->m_pSprite,
-				irr::core::round32(pObj->m_posx + pObj->m_translation.X),
-				irr::core::round32(pObj->m_posy + pObj->m_translation.Y), pTargetBuf);
+			if (pObj->m_bActive) {
+				tge_sprite::put(pObj->m_pSprite,
+					irr::core::round32(pObj->m_posx + pObj->m_translation.X),
+					irr::core::round32(pObj->m_posy + pObj->m_translation.Y), pTargetBuf);
+			}
 		}
 	}
 
@@ -229,6 +252,77 @@ namespace cs2018prj {
 					irr::core::round32(pObj->m_posy + pObj->m_translation.Y), pTargetBuf);
 			}
 			
+		}
+	}
+
+	namespace attackObject_frameBurster {
+		void Init(S_GAMEOBJECT *pObj, double _x, double _y, double _dbSpeed,
+			tge_sprite::S_SPRITE_OBJECT *pSpr)
+		{
+			pObj->m_posx = _x;
+			pObj->m_posy = _y;
+			pObj->m_dbSpeed = _dbSpeed;
+			pObj->m_nFSM = 0;
+			pObj->m_pSprite = pSpr;
+			pObj->m_vDir = irr::core::vector2df(1, 0);
+			pObj->m_pTargetObj = NULL;
+			pObj->m_bActive = false;
+		}
+
+		void Apply(S_GAMEOBJECT *pObj, double _deltaTick)
+		{
+			switch (pObj->m_nFSM)
+			{
+			case 0:
+				break;
+			case 10:
+				pObj->m_bActive = true;
+				pObj->m_nFSM = 11;
+				pObj->m_dbWorkTick = 0;
+				break;
+			case 11:
+			{
+				pObj->m_dbWorkTick += _deltaTick;
+				pObj->m_posx += _deltaTick * pObj->m_dbSpeed * pObj->m_vDir.X;
+				pObj->m_posy += _deltaTick * pObj->m_dbSpeed * pObj->m_vDir.Y;
+
+				if (pObj->m_dbWorkTick > 5.0) {
+					pObj->m_nFSM = 0;
+					pObj->m_bActive = false;
+				}
+				if (pObj->m_pTargetObj) {
+					cs2018prj::ailenObject::S_GAMEOBJECT *pTargetObj =
+						(cs2018prj::ailenObject::S_GAMEOBJECT *)pObj->m_pTargetObj;
+
+					irr::core::vector2df a = irr::core::vector2df(pObj->m_posx, pObj->m_posy);
+					irr::core::vector2df b =
+						irr::core::vector2df(pTargetObj->m_posx, pTargetObj->m_posy);
+
+					double fDist = a.getDistanceFrom(b);
+					//printf("%lf ", fDist);
+					if (fDist < 3) {
+						pObj->m_nFSM = 0;
+						pObj->m_bActive = false;
+						pTargetObj->m_nFSM = 100;
+					}
+				}
+			}
+			break;
+			default:
+				break;
+			}
+			
+
+		}
+
+		void Render(S_GAMEOBJECT *pObj, CHAR_INFO *pTargetBuf)
+		{
+			if (pObj->m_bActive) {
+				tge_sprite::put(pObj->m_pSprite,
+					irr::core::round32(pObj->m_posx + pObj->m_translation.X),
+					irr::core::round32(pObj->m_posy + pObj->m_translation.Y), pTargetBuf);
+			}
+
 		}
 	}
 
--- day16/exam1/gameobject.h
+++ day16/exam1/gameobject.h
@@ -6,6 +6,7 @@ namespace cs2018prj {
 			double m_dbSpeed;
 			double m_posx, m_posy;
 			//CHAR_INFO m_Spr;
+			void *m_pWepon;
 			tge_sprite::S_SPRITE_OBJECT *m_pSprite;
 			irr::core::vector2df m_translation;
 			int m_nFSM;
@@ -46,6 +47,24 @@ namespace cs2018prj {
 			irr::core::vector2df m_translation;
 			double m_dbWorkTick;
 			cs2018prj::playerObject::S_GAMEOBJECT *m_pTargetObj;
+			int m_nFSM;
+			bool m_bActive;
+		};
+
+		void Init(S_GAMEOBJECT *pObj, double _x, double _y, double _dbSpeed,
+			tge_sprite::S_SPRITE_OBJECT *pSpr);
+		void Apply(S_GAMEOBJECT *pObj, double _deltaTick);
+		void Render(S_GAMEOBJECT *pObj, CHAR_INFO *pTargetBuf);
+	}
+	namespace attackObject_frameBurster {
+		struct S_GAMEOBJECT {
+			double m_dbSpeed;
+			double m_posx, m_posy;
+			irr::core::vector2df m_vDir;
+			tge_sprite::S_SPRITE_OBJECT *m_pSprite;
+			irr::core::vector2df m_translation;
+			double m_dbWorkTick;
+			void *m_pTargetObj;
 			int m_nFSM;
 			bool m_bActive;
 		};
