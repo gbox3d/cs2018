8bd72b9dbfdfb87d71e122b31a47a9986b23774d
gbox3d <gbox3d@gmail.com>
2018-07-16 11:56:34 +0900

    claw object

--- day16/exam1/gameObject.cpp
+++ day16/exam1/gameObject.cpp
@@ -86,6 +86,7 @@ namespace cs2018prj {
 			pObj->m_nFSM = 0;
 			pObj->m_pSprite = pSpr;
 			pObj->m_vDir = irr::core::vector2df(1, 0);
+			pObj->m_pTargetObj = NULL;
 		}
 
 		void Apply(S_GAMEOBJECT *pObj, double _deltaTick)
@@ -96,7 +97,7 @@ namespace cs2018prj {
 				pObj->m_nFSM = 10;
 				pObj->m_dbWorkTick = 0;
 				break;
-			case 10:
+			case 10: //순찰모드 
 				pObj->m_posx += (pObj->m_dbSpeed * _deltaTick) * pObj->m_vDir.X;
 				pObj->m_posy += (pObj->m_dbSpeed * _deltaTick) * pObj->m_vDir.Y;
 				pObj->m_dbWorkTick += _deltaTick;
@@ -105,6 +106,24 @@ namespace cs2018prj {
 					pObj->m_vDir.rotateBy(180);
 					pObj->m_dbWorkTick = 0;
 				}
+				if (pObj->m_pTargetObj) {
+					irr::core::vector2df a(pObj->m_posx, pObj->m_posy);
+					irr::core::vector2df b(pObj->m_pTargetObj->m_posx, pObj->m_pTargetObj->m_posy);
+					double fDist = a.getDistanceFrom(b);
+					if (fDist < 5) {
+						pObj->m_nFSM = 20;
+					}
+				}
+				break;
+			case 20: //추적모드 
+			{
+				irr::core::vector2df a(pObj->m_posx, pObj->m_posy);
+				irr::core::vector2df b(pObj->m_pTargetObj->m_posx, pObj->m_pTargetObj->m_posy);
+				irr::core::vector2df c = b - a;
+				c.normalize();
+				pObj->m_posx += c.X * pObj->m_dbSpeed * _deltaTick;
+				pObj->m_posy += c.Y * pObj->m_dbSpeed * _deltaTick;
+			}
 				break;
 			default:
 				break;
@@ -116,6 +135,36 @@ namespace cs2018prj {
 			tge_sprite::put(pObj->m_pSprite,
 				irr::core::round32(pObj->m_posx + pObj->m_translation.X),
 				irr::core::round32(pObj->m_posy + pObj->m_translation.Y), pTargetBuf);
+		}
+	}
+
+	namespace attackObject_claw {
+		void Init(S_GAMEOBJECT *pObj, double _x, double _y, double _dbSpeed,
+			tge_sprite::S_SPRITE_OBJECT *pSpr)
+		{
+			pObj->m_posx = _x;
+			pObj->m_posy = _y;
+			pObj->m_dbSpeed = _dbSpeed;
+			pObj->m_nFSM = 0;
+			pObj->m_pSprite = pSpr;
+			pObj->m_vDir = irr::core::vector2df(1, 0);
+			pObj->m_pTargetObj = NULL;
+			pObj->m_bActive = false;
+		}
+
+		void Apply(S_GAMEOBJECT *pObj, double _deltaTick)
+		{
+			
+		}
+
+		void Render(S_GAMEOBJECT *pObj, CHAR_INFO *pTargetBuf)
+		{
+			if (pObj->m_bActive) {
+				tge_sprite::put(pObj->m_pSprite,
+					irr::core::round32(pObj->m_posx + pObj->m_translation.X),
+					irr::core::round32(pObj->m_posy + pObj->m_translation.Y), pTargetBuf);
+			}
+			
 		}
 	}
 
--- day16/exam1/gameobject.h
+++ day16/exam1/gameobject.h
@@ -25,7 +25,26 @@ namespace cs2018prj {
 			tge_sprite::S_SPRITE_OBJECT *m_pSprite;
 			irr::core::vector2df m_translation;
 			double m_dbWorkTick;
+			cs2018prj::playerObject::S_GAMEOBJECT *m_pTargetObj;
 			int m_nFSM;
+		};
+
+		void Init(S_GAMEOBJECT *pObj, double _x, double _y, double _dbSpeed,
+			tge_sprite::S_SPRITE_OBJECT *pSpr);
+		void Apply(S_GAMEOBJECT *pObj, double _deltaTick);
+		void Render(S_GAMEOBJECT *pObj, CHAR_INFO *pTargetBuf);
+	}
+	namespace attackObject_claw {
+		struct S_GAMEOBJECT {
+			double m_dbSpeed;
+			double m_posx, m_posy;
+			irr::core::vector2df m_vDir;
+			tge_sprite::S_SPRITE_OBJECT *m_pSprite;
+			irr::core::vector2df m_translation;
+			double m_dbWorkTick;
+			cs2018prj::playerObject::S_GAMEOBJECT *m_pTargetObj;
+			int m_nFSM;
+			bool m_bActive;
 		};
 
 		void Init(S_GAMEOBJECT *pObj, double _x, double _y, double _dbSpeed,
